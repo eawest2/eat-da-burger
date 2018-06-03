d8cf635971bf48a0e0d2feb9dd2622a04ed72327
unknown <eawest2@gmail.com>
2018-06-03 18:29:49 -0400

    Updated commit. Handlebars working, JS now working, Passing through to SQL, SQL errors. CSS not loading

--- models/burger.js
+++ models/burger.js
@@ -9,15 +9,15 @@ var burger = {
             cb(res);
         });
     },
-    create: function(name, cb) {
-        orm.create("burgers", name, function(res){
-            cb(res);
+    create: function(cols, vals, callback) {
+        orm.create("burgers", cols, vals, function(res) {
+            callback(res);
         });
     },
-    update: function(id, cb){
-        orm.update("burgers", id, function(res) {
-            cb(res);
-    });
+    update: function(objColVals, condition, callback) {
+        orm.update("burgers", objColVals, condition, function(res) {
+            callback(res);
+        });
     }
 };
 
