f03b76b5afdd3b911925695914f13fa47838698f
unknown <eawest2@gmail.com>
2018-06-04 17:55:53 -0400

    final push

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  user: "test",
+  user: "root",
   password: "",
   database: "burgers_db"
 });
